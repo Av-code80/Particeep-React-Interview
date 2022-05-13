b649b00f192a1fd8d580865607283c661da79cef
avehghasemian <av.ghasemian@gmail.com>
2022-05-13 11:33:54 +0200

    Style optimisation

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import MovieType from "../../types/MoviesType";
 import { ThumbsUp, ThumbsDown } from "react-feather";
-import cn from "classnames"
+import cn from "classnames";
 import Button from "../Button/Button";
 
 interface MovieCardProps {
@@ -11,55 +11,52 @@ interface MovieCardProps {
   handleDelete: () => void;
 }
 
-const MovieCard: React.FC<MovieCardProps> = ({ movie, handleLike, handleDisLike, handleDelete }) => {
+const MovieCard: React.FC<MovieCardProps> = ({
+  movie,
+  handleLike,
+  handleDisLike,
+  handleDelete,
+}) => {
   const [action, setAction] = useState<{ name?: "like" | "disLike" }>();
 
   const onLikeClick = () => {
     if (!!action && action.name == "like") {
-      handleLike(false)
-      setAction(undefined)
-    }
-    else if (!!action && action.name == "disLike") {
-      handleDisLike(false)
+      handleLike(false);
+      setAction(undefined);
+    } else if (!!action && action.name == "disLike") {
+      handleDisLike(false);
       handleLike(true);
-      setAction({ name: "like" })
-    }
-    else {
       setAction({ name: "like" });
-      handleLike(true)
+    } else {
+      setAction({ name: "like" });
+      handleLike(true);
     }
-  }
+  };
 
   const onDisLikeClick = () => {
     if (!!action && action.name == "disLike") {
-      handleDisLike(false)
-      setAction(undefined)
-    }
-    else if (!!action && action.name == "like") {
-      handleLike(false)
+      handleDisLike(false);
+      setAction(undefined);
+    } else if (!!action && action.name == "like") {
+      handleLike(false);
       handleDisLike(true);
-      setAction({ name: "disLike" })
-    }
-    else {
-      handleDisLike(true)
+      setAction({ name: "disLike" });
+    } else {
+      handleDisLike(true);
       setAction({ name: "disLike" });
     }
-  }
+  };
 
   const onDeleteClick = () => {
-    handleDelete()
-  }
+    handleDelete();
+  };
 
   return (
     <div>
       <div className="flex flex-col overflow-hidden items-center bg-white rounded-lg border shadow-md p-4">
         <div className="flex flex-col h-full leading-normal w-full gap-3">
           <img
-<<<<<<< HEAD
             className="w-full h-[18rem] rounded-md ease-in-out duration-300 scale-100 md:h-[20rem] hover:scale-105"
-=======
-            className="w-full h-[20rem] rounded-md ease-in-out duration-300 scale-100 hover:scale-105"
->>>>>>> 6d4369eb3f1df6251d6ff7c9f500810ea04a4b8a
             src={require(`../../assets/images/${movie.image}`)}
             alt=""
           ></img>
